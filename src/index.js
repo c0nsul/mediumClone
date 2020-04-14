a27f127917629a8151c328cd2d6a24166ad3a5c0
ConsuL <consul.developer@gmail.com>
2020-04-14 11:21:44 +0300

    ohmybrain get user with token user status checker

--- src/index.js
+++ src/index.js
@@ -3,16 +3,20 @@ import ReactDOM from 'react-dom'
 import {BrowserRouter as Router} from 'react-router-dom'
 
 import Routes from 'routes'
-import TopBar from 'components/topBar'
 import {CurrentUserProvider} from 'contexts/currentUser'
+//component - no Brase! {}
+import TopBar from 'components/topBar'
+import CurrentUserChecker from 'components/currenUserChecker'
 
 const App = () => {
     return (
         <CurrentUserProvider>
+            <CurrentUserChecker>
             <Router>
                 <TopBar/>
                 <Routes/>
             </Router>
+            </CurrentUserChecker>
         </CurrentUserProvider>
     )
 }
